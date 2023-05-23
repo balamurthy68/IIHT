c46ac7e6d0b6cd4a0a189a3e56422edb72eb6bcf
balamurthy68 <43419549+balamurthy68@users.noreply.github.com>
2023-05-23 07:28:24 +0530

    Add files via upload

---
+++ UseCaseMS2.java
@@ -0,0 +1,76 @@
+package com.iiht.evaluation.automation.functional;
+
+
+public class UseCaseMS2 {
+
+	public static void preset() {
+
+	}
+
+
+	public static void testMouseOverPersonalFinance()  {
+
+	}
+
+	public static void testClickTools()  {
+
+	}
+
+	public static void testHomeLoanEMICalculator()  {
+
+	}
+
+
+	public void TestEnterValueLoanAmount() 	{
+	}
+	public void TestEnterValueLoanPeriod() 
+	{
+	}
+	public void TestSelectEMIStartsFrom() 
+	{
+	}
+	public void TestEnterInterestRate() 
+	{
+	}
+	public void TestEnterValueUpfrontCharges() throws InterruptedException
+	{
+	}
+	public void TestClickSubmit() throws InterruptedException
+	{
+	}
+	public void TestPrintTotalPayment() throws InterruptedException
+	{
+	}
+	public void TestPrintEMI() throws InterruptedException
+	{
+	}
+
+	public void TestPrintTableDetails7thYearEMIPaymentInTheYear() throws InterruptedException
+	{
+
+
+	}
+	public void TestPrintTableDetails7thYearInterestPaymentInTheYear() throws InterruptedException
+	{
+
+
+	}
+	public void TestPrintTableDetails7thYearPrincipalPaymentInTheYear() throws InterruptedException
+	{
+
+
+	}
+	public void TestPrintTableDetails5thYearOutstandingPrincipalAtTheEndOfYear() throws InterruptedException
+	{
+
+
+	}
+	public static void closeAll() {
+	}
+
+
+
+
+}
+
+
